58dd21136015df75e7593b49a6ce839f08063def
Prajwal M V <mvprajwal546@gmail.com>
2026-03-01 12:10:57 +0530

    Props

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -1,7 +1,18 @@
+import Card from "./components/Card";
 function App() {
+
+  let myObj={
+    username:"John Doe",
+    age:30,
+  }
+
+  let myArr=[1,2,3,4,5];
+
   return (
     <center>
       <h1 className="bg-green-500 text-black p-4 rounded-xl">Hello Tailwind</h1>
+      <Card channel="My Channel"  someObject={myObj} someArray={myArr} btnText="Click Me"/>
+      <Card  channel="Another Channel" btnText="Read More"/>
     </center>
   );
 }
